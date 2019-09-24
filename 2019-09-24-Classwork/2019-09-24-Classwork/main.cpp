6faa4452afe910b3566908f1b6feb7022a86f512
lucyreyes0831 <55443270+lucyreyes0831@users.noreply.github.com>
2019-09-24 08:37:51 -0400

    Add files via upload

---
+++ 2019-09-24-Classwork/2019-09-24-Classwork/main.cpp
@@ -0,0 +1,22 @@
+// Name: Lucy Reyes
+// Intro to CS
+// Mr. Williams
+// Period 1
+// 2019-09-24-Classwork
+
+#include <iostream>
+
+int main()
+{
+	int x = 7;
+	int y = 5;
+	float b = 6;
+	float c = 8;
+	char a = 9;
+	char z = 4;
+	bool l = 1;
+	bool k = 10;
+	double j = 3;
+	double r = 2;
+	std::cout << sizeof(x) << std::endl;
+}
