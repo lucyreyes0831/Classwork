371d084f4e188b2d204ea21616abb32e3b17f6e3
lucyreyes0831 <55443270+lucyreyes0831@users.noreply.github.com>
2019-10-02 08:38:58 -0400

    Add files via upload

---
+++ 2019-10-02-Classwork/2019-10-02-Classwork/main.cpp
@@ -0,0 +1,21 @@
+// Name: Lucy Reyes
+// Intro to CS
+// Mr. Williams
+// Period 1
+// 2019-10-02-Classwork
+
+
+int main()
+{
+	char letterA = 'A';
+	char b = 'h';
+	char lmnop = 67;
+	char myName = 'l';
+	
+	short life = 1;
+	int  periods = 10;
+	unsigned long reyes = 1000000;
+	
+	bool isHot = true;
+	bool isItFire = false;
+} 
