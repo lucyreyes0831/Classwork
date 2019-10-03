6ba537542823aacd03af0f76b63f9dca6835dc96
lucyreyes0831 <55443270+lucyreyes0831@users.noreply.github.com>
2019-10-03 08:41:00 -0400

    Add files via upload

---
+++ 2019-10-3-Classwork/2019-10-3-Classwork/main.cpp
@@ -0,0 +1,16 @@
+// Name: Lucy Reyes
+// Intro to CS
+// Period 1
+// Mr. Williams
+// 2019-10-3-Classwork
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Enter your favorite number: ";
+	int 31;
+	cin >> 31;
+	cout << "Cool," << 31 << "is my favorite number too!" << endl;
+}
